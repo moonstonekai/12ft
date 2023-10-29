6f4fc2ca76dd58575babc7ec55b8ef5fb180e4b1
Kai Prol <138074380+moonstonekai@users.noreply.github.com>
2023-10-29 14:33:16 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import flask
 import requests
-from flask import request
 
 app = flask.Flask(__name__)
 googlebot_headers = {
@@ -13,7 +12,6 @@ def bypass_paywall(url):
     Bypass paywall for a given url
     """
     response = requests.get(url, headers=googlebot_headers)
-    response.encoding = response.apparent_encoding
     return response.text
 
 
@@ -25,29 +23,7 @@ def main_page():
 @app.route("/article", methods=["POST"])
 def show_article():
     link = flask.request.form["link"]
-    try:
-        return bypass_paywall(link)
-    except requests.exceptions.RequestException as e:
-        return str(e), 400
-    except e:
-        raise e
+    return bypass_paywall(link)
 
-@app.route("/", defaults={"path": ""})
-@app.route('/<path:path>', methods=["GET"])
-def get_article(path):
-    print(path)
-    full_url = request.url
-    parts = full_url.split('/',4)
-    if len(parts) >= 5:
-        actual_url = 'https://' + parts[4].lstrip('/')
-        print(actual_url)
-        try:
-            return bypass_paywall(actual_url)
-        except requests.exceptions.RequestException as e:
-            return str(e), 400
-        except e:
-            raise e
-    else:
-        return "Invalid URL", 400
 
-app.run(debug=False)
+app.run(debug=True)
